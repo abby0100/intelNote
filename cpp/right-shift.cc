e40bca5c9a77b6e130618916840a5b32427ead3c
xiongyuan <yuan.xiong@intel.com>
2016-12-02 14:21:00 +0800

    Update for apktool

---
+++ cpp/right-shift.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	
+	int a =2, b =3;
+	std::cout << "a=" << a << " b=" << b;
+	std::cout << std::endl;
+
+	std::cout << "a << b " << (a << b);
+	std::cout << std::endl;
+	std::cout << "b << a " << (b << a);
+	std::cout << std::endl;
+}
